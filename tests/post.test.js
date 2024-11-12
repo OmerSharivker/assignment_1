37c23f7b53b188db9e1fc7e395b0cd054d6bee0d
Omer Sharivker <sharivker90@gmail.com>
2024-11-12 12:53:20 +0200

    test-api-3

--- tests/post.test.js
+++ tests/post.test.js
@@ -1,7 +1,7 @@
 const request=require('supertest')
 const app =require('../app')
 const mongoose =require('mongoose')
-
+let id="";
 
 beforeAll(async () =>{
    
@@ -16,4 +16,21 @@ describe("Posts tests",()=>{
         const res = await request(app).get('/api/posts');
         expect(res.statusCode).toEqual(200)
     })
+    test("save post" , async ()=>{
+        const newPostData = {
+            message: 'This is a test post',
+            sender: '12345'
+          };
+        const res = await request(app).post('/api/posts').send(newPostData)
+        id=res.body._id
+        expect(res.statusCode).toEqual(201)
+        expect(res.body.message).toEqual(newPostData.message)
+        expect(res.body.sender).toEqual(newPostData.sender)
+    })
+    test("get post by id" , async ()=>{
+        const res = await request(app).get(`/api/posts/${id}`)
+        expect(res.statusCode).toEqual(200)
+        expect(res.body.post._id).toEqual(id)
+    })
+
 })
