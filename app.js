b4a0db4fe9e31e2e91dbe50ace64a25d2bd1e647
Omer Sharivker <sharivker90@gmail.com>
2024-11-12 12:29:36 +0200

    test-1

--- app.js
+++ app.js
@@ -7,18 +7,11 @@ const app =express();
 //middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}))
-const port =process.env.PORT;
 
 app.get('/',(req,res)=>{
     res.send("hello")
  })
- 
  dbConnect();
-
  app.use('/api', require('./routes/postRoutes.js'));
 
-
-
-app.listen(port, () =>{
-    console.log(`server is running on port ${port}`)
-})
+ module.exports=app
