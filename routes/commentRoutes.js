d4bae860ac012a6f92e997b9a9e189cf9f4e877e
shlomi65436 <shlomi65436@gmail.com>
2024-11-14 14:40:47 +0200

    api23-comments

--- routes/commentRoutes.js
+++ routes/commentRoutes.js
@@ -4,7 +4,8 @@ const commentsControllers = require('../controllers/commentsControllers');
 const router = express.Router();
 
 
-router.post('/comment',commentsControllers.postComment)
-
+router.post('/comment',commentsControllers.postComment);
+router.get('/comment/:commentId',commentsControllers.readComment);
+router.put('/comment/:commentId',commentsControllers.updateComment);
 
 module.exports = router;
