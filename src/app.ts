38e8facc07652ef0acdb38cc8cf7922736a1a7cb
Omer Sharivker <sharivker90@gmail.com>
2024-12-30 19:46:21 +0200

    sdfs

--- src/app.ts
+++ src/app.ts
@@ -1,29 +1,34 @@
 import express from 'express';
-import  {dbConnect}  from './utils/db';
+import { dbConnect } from './utils/db';
 import bodyParser from 'body-parser';
 import postRoutes from './routes/postRoutes';
 import commentRoutes from './routes/commentRoutes';
 import authRoutes from './routes/authRoutes';
 import swaggerUi from 'swagger-ui-express';
 import swaggerDocs from './swaggerConfig';
+import cors from 'cors'; // Import the cors package
 require('dotenv').config();
 
-const app =express();
-//middleware
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}))
+const app = express();
 
-app.get('/',(req,res)=>{
-    res.send("hello")
- })
- dbConnect();
+// Enable CORS for all routes
+app.use(cors());
+
+// Middleware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+    res.send("hello");
+});
+
+dbConnect();
 
 app.use('/api/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 app.use('/api', postRoutes);
 app.use('/api', commentRoutes);
-app.use('/api',  authRoutes);
+app.use('/api', authRoutes);
 
 export default app;
-
 
 
