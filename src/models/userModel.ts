f55f0410eff5239fffabfbe6f7364476ccc62761
Omer Sharivker <sharivker90@gmail.com>
2024-12-31 10:29:13 +0200

    dfsdfs

--- src/models/userModel.ts
+++ src/models/userModel.ts
@@ -1,12 +1,28 @@
 import mongoose, { Schema, Document } from 'mongoose';
 
 
-
-const UserSchema: Schema = new Schema({
-    email: { type: String, required: true, unique: true },
-    password:{type: String , required: true},
-    refreshTokens: { type: [String], default: [] }
-});
+const UserSchema =new Schema({
+    userName:{
+        type: String,
+        required :true,
+    },
+    email:{
+        type: String,
+        required :true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required :true
+    },
+    refreshTokens: {
+         type: [String], default: [] 
+    },
+    image:{
+        type: String,
+        default: ''
+    }
+})
 
 const User = mongoose.model('users', UserSchema);
 
